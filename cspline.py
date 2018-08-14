61d49a9ee38c2b7284c3ffc2de3733f19910211c
cl0ete <cloete.dupreez@gmail.com>
2018-08-14 13:03:07 +0200

    Add files via upload

--- cspline.py
+++ cspline.py
@@ -1,37 +1,35 @@
 import numpy as np
-import lu
-
+import LU
 
 def curvatures(xdata, ydata):
-	'''return curvatures of a cubic spline at its knots'''
-	n = len(xdata) - 1
+	""" return the curvatures of a cubic spline at its knots """
+	n = len(xdata)-1
 	c = np.zeros(n)
 	d = np.ones(n + 1)
 	e = np.zeros(n)
-	k = np,zeros(n + 1)
-	
+	k = np.zeros(n + 1)
+
 	c[0:n-1] = xdata[0:n-1] - xdata[1:n]
-	d[1:n] = 2.0 * (xdata[0:n-1] - xdata[2:n+1])
+	d[1:n] = 2.0 *(xdata[0:n-1] - xdata[2:n+1])
 	e[1:n] = xdata[1:n] - xdata[2:n+1]
-	k[1:n] = 6.0 * (ydata[0:n-1] - ydata[1:n] / \
+	k[1:n] = 6.0*(ydata[0:n-1] - ydata[1:n]) / \
 				(xdata[0:n-1] - xdata[1:n]) \
-				-6.0 * (ydata[1:n]-ydata[2:n+1]) \
+			-6.0 * (ydata[1:n] - ydata[2:n+1]) / \
 				(xdata[1:n] - xdata[2:n+1])
 
-	lu.lu_decomp3(c,d,e)
-	lu,lu_solve(c, d, e, k)
-
+	LU.lu_decomp3(c,d,e)
+	LU.lu_solve3(c,d,e,k)
 	return k
 
-def eval_spline(xdata, ydata k,x):
-	'''Evaluate the cubic spline at x. The curavtures k can be computed with the function curvatures'''
-	
+def eval_spline(xdata, ydata, k, x):
+	""" Evaluate the cubic spline at x. The curvatures k can me computed with
+		the functio curvatures """
 	def find_segment(xdata, x):
 		pass
 
 	i = find_segment(xdata, x)
 	h = xdata[i] - xdata[i+1]
-	y = ((x - xdata[i+1])**3/h)(x - xdata[i+1]) * h) * k[i]/6.0 \
-				- ((x - xdata[i])**3/h - (x -xdata[i]) * h)  * k[i+1]/6.0 \
-				+ ydata[i] * (x - xdata[i+1]) \
-				- ydata[i +1] * (x -xdata[i])/ h
+	y = ((x - xdata[i+1]) ** 3/h - (x - xdata[i+1]) * h) * k[i]/6.0 \
+		- ((x - xdata[i]) ** 3/h - (x - xdata[i]) * h) * k[i+1]/6.0 \
+		+ ydata[i] * (x - xdata[i+1])\
+		- ydata[i+1] * (x -xdata[i])/h
